b50f8fe6759398d5e576993bbc3666c5ce420e29
SathyaRam <50044265+SathyaseelanRamachandran@users.noreply.github.com>
2021-10-08 22:32:08 +0800

    exports module

---
+++ config/development.js
@@ -0,0 +1,21 @@
+
+//const mongoose = require('mongoose');
+
+//const db = "mongodb+srv://sathya:sairam5779@cluster0.z1syl.mongodb.net/Weather?retryWrites=true&w=majority";
+//mongoose
+//.connect(db) //{useNewUrlParser: true, useUnifiedTopology: true}
+    
+//.then(() => {
+   // console.log("Connected to database");
+//}
+//)
+
+//.catch(() => {
+  //  console.log("Error connecting to database");
+//}
+//)
+
+module.exports = {
+MODE: 'development',
+DATA_DIR: '/data'
+};
